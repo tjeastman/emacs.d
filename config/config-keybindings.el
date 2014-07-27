0d0b74801837e506eec4de9e847f040225c6e3ba
Thomas Eastman <tjeastman@gmail.com>
2014-07-26 21:20:55 -0700

    additional smex keybindings

--- config/config-keybindings.el
+++ config/config-keybindings.el
@@ -1,2 +1,5 @@
 (global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 (provide 'config-keybindings)
