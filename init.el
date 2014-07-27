ad7ad6feba48650bfd6a105e79fe2f972757adf6
Thomas Eastman <tjeastman@gmail.com>
2014-07-26 19:28:39 -0700

    configure package manager and install some packages by default

--- init.el
+++ init.el
@@ -4,3 +4,17 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+; configure packages
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-user-dir (expand-file-name "packages" user-emacs-directory))
+
+; install required packages
+(package-initialize)
+(package-refresh-contents)
+(dolist (package-name '(cask pallet))
+  (unless (package-installed-p package-name)
+    (package-install package-name)))
