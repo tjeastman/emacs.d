5a18058891f7bc2e0e31e883cfbf46c4ef994da2
Thomas Eastman <tjeastman@gmail.com>
2014-07-26 22:52:45 -0700

    improved ido configuration

--- config/config-features.el
+++ config/config-features.el
@@ -1,4 +1,18 @@
-(ido-mode)
+(require 'ido)
+(require 'ido-ubiquitous)
+(require 'flx-ido)
+
+(setq ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-ignore-extensions t
+      ido-auto-merge-work-directories-length -1
+      ido-save-directory-list-file (expand-file-name "ido.last" user-emacs-state-directory)
+      ido-use-faces nil)
+(ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous-mode 1)
+(flx-ido-mode +1)
 
 (require 'smex)
 (setq smex-save-file
