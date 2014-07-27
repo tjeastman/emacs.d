fd0969dddf94c10a4e123bad60634e1747c52bc8
Thomas Eastman <tjeastman@gmail.com>
2014-07-26 21:05:53 -0700

    configure handling of backup files

--- config/config-editor.el
+++ config/config-editor.el
@@ -4,4 +4,13 @@
 (setq save-place-file
       (expand-file-name "places" user-emacs-state-directory))
 
+; store backup files in a central directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name "backups" user-emacs-directory))))
+(setq backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
 (provide 'config-editor)
