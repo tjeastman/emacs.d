8f79be1ea159434f9633998685b0ed9c4e84e45a
Thomas Eastman <tjeastman@gmail.com>
2014-07-26 20:56:52 -0700

    add directory for state files and ignore it

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
+; organize emacs feature state files
+(setq user-emacs-state-directory
+      (expand-file-name "state" user-emacs-directory))
+
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 (require 'config-packages)
 (require 'config-keybindings)
