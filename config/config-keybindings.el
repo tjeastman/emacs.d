1fd322cfef2ebabcbce78728896afbd84508f711
Thomas Eastman <tjeastman@gmail.com>
2014-08-16 14:58:38 -0700

    use ibuffer instead of list-buffers

--- config/config-keybindings.el
+++ config/config-keybindings.el
@@ -2,6 +2,8 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 (global-set-key [remap move-beginning-of-line]
