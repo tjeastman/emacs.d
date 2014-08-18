a239418706ddbb5188b37f2db0bb7b3a163a8548
Thomas Eastman <tjeastman@gmail.com>
2014-08-17 21:46:34 -0700

    add keybinding for backward-kill-word

--- config/config-keybindings.el
+++ config/config-keybindings.el
@@ -4,6 +4,8 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+(global-set-key (kbd "C-;") 'backward-kill-word)
+
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 (global-set-key [remap move-beginning-of-line]
