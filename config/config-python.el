956559a23b05fca0f910f6683e623b8e7cb166a3
Thomas Eastman <tjeastman@gmail.com>
2017-11-11 22:30:41 -0800

    drop pymacs and ropemacs

--- config/config-python.el
+++ config/config-python.el
@@ -11,13 +11,6 @@
   :init
   (add-hook 'python-mode-hook 'jedi:setup))
 
-;; enable access to rope refactoring library for Python through Pymacs
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-guess-project t)
-(setq ropemacs-enable-autoimport t)
-(setq ropemacs-autoimport-modules '("os" "sys"))
-
 (add-hook 'python-mode-hook (lambda () (subword-mode +1)))
 
 (use-package virtualenvwrapper)
