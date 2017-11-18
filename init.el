761f12f79efc6f5e1f0e2a10013729fec602e8d6
Thomas Eastman <teastman@23andme.com>
2017-11-18 10:55:29 -0800

    remove unused contrib subdirectory from load-path

--- init.el
+++ init.el
@@ -1,22 +1,13 @@
-; (package-initialize)
+;; (package-initialize)
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message nil)
 
-; organize emacs feature state files
+;; organize emacs feature state files
 (setq user-emacs-state-directory
       (expand-file-name "state" user-emacs-directory))
 
-(defun add-top-level-subdirectories-to-load-path (&rest top-level-dirs)
-  "Augment load-path with the top-level subdirectories of a list of directories."
-  (dolist (top-level-dir top-level-dirs)
-    (dolist (top-level-subdir (directory-files top-level-dir t "\\w+"))
-      (add-to-list 'load-path top-level-subdir))))
-
-(add-top-level-subdirectories-to-load-path
- (expand-file-name "contrib" user-emacs-directory))
-
-; store customizations outside of the emacs configuration directory
+;; store customizations outside of the emacs configuration directory
 (setq custom-file "~/.emacs-custom.el")
 (load custom-file t)
 
