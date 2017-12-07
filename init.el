7f69ea0384bd537704d24a7013c84d808b4b4b58
Thomas Eastman <tjeastman@gmail.com>
2017-12-07 01:35:09 -0800

    dynamically add contrib/ subdirectories to load-path

--- init.el
+++ init.el
@@ -20,6 +20,14 @@
 (load custom-file t)
 
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+
+;; add subdirectories of contrib to the load path...
+(let ((contrib-dir (expand-file-name "contrib" user-emacs-directory)))
+  (if (file-directory-p contrib-dir)
+      (dolist (contrib-subdir (directory-files contrib-dir))
+        (unless (member contrib-subdir '("." ".."))
+          (add-to-list 'load-path (expand-file-name contrib-subdir contrib-dir))))))
+
 (require 'config-packages)
 (require 'config-functions)
 (require 'config-appearance)
