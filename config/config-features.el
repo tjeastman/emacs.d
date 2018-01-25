40dc62fb6c286e04df94c7af203322e3c572c7c7
Thomas Eastman <tjeastman@gmail.com>
2018-01-24 19:43:35 -0800

    update ivy directory configuration to include . and .. in completions

--- config/config-features.el
+++ config/config-features.el
@@ -15,7 +15,6 @@
   :custom
   (ivy-use-virtual-buffers t)
   (ivy-display-style 'fancy)
-  (ivy-extra-directories nil)
   :config
   (setq ivy-re-builders-alist
         '((swiper . ivy--regex-plus)
