2e92f90468149f171dadf179d3f9e8b7d272c88c
Thomas Eastman <teastman@23andme.com>
2018-08-07 23:40:03 -0700

    enable the menu bar on MacOS

--- config/config-appearance.el
+++ config/config-appearance.el
@@ -6,9 +6,11 @@
 
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
 (if (fboundp 'fringe-mode) (fringe-mode 0))
+
+(if (and (fboundp 'menu-bar-mode) (not (eq system-type 'darwin)))
+    (menu-bar-mode -1))
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
