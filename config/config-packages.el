765e9df773c0053398f785b45706756d1222cfcd
Thomas Eastman <teastman@23andme.com>
2018-08-09 23:31:11 -0700

    simplify installation of use-package package

--- config/config-packages.el
+++ config/config-packages.el
@@ -13,18 +13,9 @@
 
 (package-initialize)
 
-(defvar config-required-packages
-  '(use-package)
-  "List of packages that are automatically installed.")
-
-;; ensure that all required packages are installed
-(let ((package-not-installed-p (lambda (pkg) (not (package-installed-p pkg)))))
-  (if (delq nil (mapcar package-not-installed-p config-required-packages))
-      (progn
-        (package-refresh-contents)
-        (dolist (package-name config-required-packages)
-          (unless (package-installed-p package-name)
-            (package-install package-name))))))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (use-package use-package
   :custom
