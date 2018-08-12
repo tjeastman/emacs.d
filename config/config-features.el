27afbb9892cbdce56f9a65c9f184ffbb991caa43
Thomas Eastman <tjeastman@gmail.com>
2018-08-12 13:13:04 -0700

    fix :bind section for multiple-cursors

--- config/config-features.el
+++ config/config-features.el
@@ -138,10 +138,10 @@
 
 (use-package multiple-cursors
   :bind
-  ("C-S-c C-S-c" . mc/edit-lines)
-  ("C->" . 'mc/mark-next-like-this)
-  ("C-<" . 'mc/mark-previous-like-this)
-  ("C-c C-<" . 'mc/mark-all-like-this))
+  (("C-S-c C-S-c" . mc/edit-lines)
+   ("C->" . 'mc/mark-next-like-this)
+   ("C-<" . 'mc/mark-previous-like-this)
+   ("C-c C-<" . 'mc/mark-all-like-this)))
 
 ;; FIX: not enabled in python-mode
 (use-package electric-operator
