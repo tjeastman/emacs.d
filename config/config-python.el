799d775e775c07f250f0e87dc118bbd18756bdcc
Thomas Eastman <tjeastman@gmail.com>
2019-03-01 00:43:29 -0800

    add pipenv-mode to python-mode-hook in python package configuration

--- config/config-python.el
+++ config/config-python.el
@@ -4,6 +4,14 @@
 
 ;;; Code:
 
+(use-package pipenv
+  :delight
+  :commands pipenv-mode
+  :init
+  (setq
+   pipenv-projectile-after-switch-function
+   #'pipenv-projectile-after-switch-default))
+
 (use-package python
   :delight "Py"
   :mode ("\\.py\\'" . python-mode)
@@ -11,16 +19,9 @@
   (python-indent-offset 4)
   (python-indent-guess-indent-offset nil)
   :config
+  (add-hook 'python-mode-hook #'pipenv-mode)
   (add-hook 'python-mode-hook #'highlight-indentation-mode)
   (add-hook 'python-mode-hook #'electric-operator-mode))
-
-(use-package pipenv
-  :delight
-  :hook (python-mode . pipenv-mode)
-  :init
-  (setq
-   pipenv-projectile-after-switch-function
-   #'pipenv-projectile-after-switch-default))
 
 (provide 'config-python)
 ;;; config-python.el ends here
