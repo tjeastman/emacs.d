906ee0e1605bc1cd841b389c96c17265c158b030
Thomas Eastman <tjeastman@gmail.com>
2020-04-17 02:16:17 -0700

    add org-ref package

--- config/config-org.el
+++ config/config-org.el
@@ -3,4 +3,10 @@
   (org-babel-load-languages '((emacs-lisp . t)
                               (python . t))))
 
+(use-package org-ref
+  :custom
+  (org-ref-bibliography-notes "~/refs/notes.org")
+  (org-ref-default-bibliography '("~/refs/references.bib"))
+  (org-ref-pdf-directory "~/refs/docs/"))
+
 (provide 'config-org)
