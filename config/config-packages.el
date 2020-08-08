5ac8e3dc62639933fb93aea110867705108ebfa3
Thomas Eastman <tjeastman@gmail.com>
2020-08-07 18:31:06 -0700

    update MELPA URL

--- config/config-packages.el
+++ config/config-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (setq package-user-dir
       (expand-file-name "packages" user-emacs-directory))
