015dcaf502d42afb9ea71e09699ed85c5818e6b4
Thomas Eastman <tjeastman@gmail.com>
2020-11-24 14:59:42 -0800

    explicitly declare whitespace-cleanup command from whitespace package

--- config/config-editor.el
+++ config/config-editor.el
@@ -170,6 +170,8 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 
 ;; visualize unwanted whitespace characters and lines that are too long
 (use-package whitespace
+  :ensure nil
+  :commands whitespace-cleanup
   :custom
   (whitespace-line-column 100)
   (whitespace-style '(face tabs empty trailing lines-tail)))
