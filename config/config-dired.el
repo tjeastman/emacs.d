912c4a8cef21cb8ad7bfbff4c49787d256fadec8
Thomas Eastman <tjeastman@gmail.com>
2022-06-20 22:43:34 -0700

    remove unused dired-rsync package

--- config/config-dired.el
+++ config/config-dired.el
@@ -8,15 +8,4 @@
   (if (eq system-type 'darwin)
       (setq dired-use-ls-dired nil)))
 
-(make-variable-buffer-local 'my-dired-rsync-destination)
-(use-package dired-rsync
-  :bind
-  (:map dired-mode-map ("C-c C-r" . my-dired-rsync))
-  :init
-  (defun my-dired-rsync ()
-    (interactive)
-    (if (and (boundp 'my-dired-rsync-destination) my-dired-rsync-destination)
-        (dired-rsync my-dired-rsync-destination)
-      (message "rsync destination is not set"))))
-
 (provide 'config-dired)
