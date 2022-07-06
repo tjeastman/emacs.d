67862899d0385aaa507c24319a58048eaedc3e69
Thomas Eastman <tjeastman@gmail.com>
2022-07-06 10:35:51 -0700

    use aliased variable inhibit-startup-screen instead of inhibit-startup-message

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
                                ;; restore after startup
                                (setq gc-cons-threshold 800000)))
 
-(setq inhibit-startup-message t)
+(setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 
 ;; store customizations outside of the emacs configuration directory
