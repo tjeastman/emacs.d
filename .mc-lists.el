794a6d77ea5b14836c8d1fd8e10cd961c730f96c
Thomas Eastman <tjeastman@gmail.com>
2023-02-26 09:03:21 -0800

    add commands to run for all cursors in multiple-cursors-mode

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,7 +3,16 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        asm-comment
+        c-electric-brace
+        c-electric-colon
+        c-electric-lt-gt
+        c-electric-paren
+        c-electric-pound
+        c-electric-semi&comma
         c-electric-slash
+        c-electric-star
+        c-indent-line-or-region
         crux-move-beginning-of-line
         indent-for-tab-command
         kill-region
